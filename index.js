905dea034f589298a89a1e4546f3c583fa853a87
Maciej Sawicki <maciej.sawicki@acaisoft.com>
2018-08-07 16:42:51 +0200

    Added scrapping interval

--- index.js
+++ index.js
@@ -5,26 +5,60 @@ const opn = require('opn');
 
 const olxUrl = 'https://www.olx.pl/nieruchomosci/mieszkania/wynajem/bialystok/?search[filter_float_price:to]=1400&search[filter_enum_rooms][0]=one&search[filter_enum_rooms][1]=two';
 
-async function parsePage() {
+let lastOffer = null;
+
+async function readPage() {
   const response = await fetch(olxUrl);
   const text = await response.text();
 
   const $ = cheerio.load(text);
-  const offerName =
-      $('#offers_table > tbody > tr:nth-child(2) > td > div > table > tbody > tr:nth-child(1) > td.title-cell > div > h3 > a > strong')
-          .text();
-  const offerPrice =
-      $('#offers_table > tbody > tr:nth-child(2) > td > div > table > tbody > tr:nth-child(1) > td.wwnormal.tright.td-price > div > p > strong')
-          .text();
-  const offerLocation =
-      $('#offers_table > tbody > tr:nth-child(2) > td > div > table > tbody > tr:nth-child(2) > td.bottom-cell > div > p > small:nth-child(1) > span')
-          .text()
-          .trim();
-  const offerUrl =
-      $('#offers_table > tbody > tr:nth-child(2) > td > div > table > tbody > tr:nth-child(1) > td.title-cell > div > h3 > a')
-          .attr('href');
 
-  sendNotification(`${offerName} ${offerPrice}`, offerLocation, offerUrl);
+  let firstOffer;
+  try {
+    firstOffer = readOfferData($, 0);
+  } catch (e) {
+    console.error('Parsing template failed!', e);
+    return
+  }
+
+  /* When lastOffer is null, it means the program just started */
+  if (lastOffer == null) {
+    console.log('Program Start!');
+    lastOffer = firstOffer;
+    return;
+  }
+
+  /* No new offer */
+  if (firstOffer.id === lastOffer.id) {
+    console.log('No new offer...');
+    return;
+  }
+
+  console.log('New Offer!!!', firstOffer);
+  sendNotification(`${firstOffer.name} ${firstOffer.offerPrice}`, firstOffer.location, firstOffer.url);
+  lastOffer = firstOffer;
+}
+
+function readOfferData($, offerIndex) {
+  const index = offerIndex + 2;
+  const offer = {
+    name:
+        $(`#offers_table > tbody > tr:nth-child(${index}) > td > div > table > tbody > tr:nth-child(1) > td.title-cell > div > h3 > a > strong`)
+            .text(),
+    price:
+        $(`#offers_table > tbody > tr:nth-child(${index}) > td > div > table > tbody > tr:nth-child(1) > td.wwnormal.tright.td-price > div > p > strong`)
+            .text(),
+    location:
+        $(`#offers_table > tbody > tr:nth-child(${index}) > td > div > table > tbody > tr:nth-child(2) > td.bottom-cell > div > p > small:nth-child(1) > span`)
+            .text()
+            .trim(),
+    url:
+        $(`#offers_table > tbody > tr:nth-child(${index}) > td > div > table > tbody > tr:nth-child(1) > td.title-cell > div > h3 > a`)
+            .attr('href'),
+  };
+  offer.id = offer.url.split('#')[1];
+
+  return offer;
 }
 
 function sendNotification(title, message, url) {
@@ -43,7 +77,11 @@ function sendNotification(title, message, url) {
 }
 
 setTimeout(async () => {
-  await parsePage();
+  await readPage()
 });
+
+setInterval(async () => {
+  await readPage();
+}, 5000);
 
 // sendNotification("Foo", "bar", "http://tibia.com/");
